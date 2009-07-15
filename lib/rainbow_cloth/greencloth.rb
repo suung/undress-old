a5e9ad3547f7ac8095c5e3af990cda16d33ae615
Álvaro Gil <zevarito@gmail.com>
2009-07-14 21:12:23 -0300

    parse greencloth links and anchors

--- lib/rainbow_cloth/greencloth.rb
+++ lib/rainbow_cloth/greencloth.rb
@@ -6,5 +6,59 @@ module RainbowCloth
   end
 
   class GreenCloth < Textile
+
+    # inline elements
+    rule_for(:a) {|e|
+      "[%s%s%s]" % process_links_and_anchors(e)
+    }
+
+    def process_links_and_anchors(e)
+      inner, name, href = e.inner_html, e.get_attribute("name"), e.get_attribute("href")
+
+      # is an anchor? and cannot be child of any h1..h6
+      if name && !e.parent.name.match(/^h1|2|3|4|5|6$/)
+        fill_with = inner == name || inner == name.gsub(/-/,"\s") ? ["# ", "#{inner}", " #"] : ["# #{inner}", " -> ", "#{name} #"]
+      # is a link to an anchor?
+      elsif href && href =~ /^\/#/
+        fill_with = ["\"#{inner}\"", ":", "#{href}"]
+      elsif href && href =~ /^#/
+        fill_with = ["#{inner}", " -> ", "#{href}"]
+      # it is an external link?
+      elsif href && href =~ /^(https?|s?ftp):\/\//
+        return href.gsub(/^(https?|s?ftp):\/\//, "") == inner ? ["#{href}", "", ""] : ["#{inner}", " -> ", "#{href}"]
+      # links starting without /
+      elsif href && href =~ /^[^\/]/
+        return ["", "#{e.inner_text}", ""]
+      # link with 3 more /
+      elsif href && href.count("/") >= 3
+        fill_with = ["#{inner}", " -> ", "#{href}"]
+      # wiki page with index
+      elsif href && href =~ /(?:\/page\/\+)[0-9]+$/
+        fill_with = ["#{inner}", " -> ", "+#{href.gsub(/\+[0-9]+$/)}"]
+      # pages or group pages
+      else
+        context_name, page_name = href.split("/")[1..2]
+        page_name = context_name if page_name.nil?
+        wiki_page_name = page_name.gsub(/[a-z-]*[^\/]$/m) {|m| m.tr('-',' ')}
+
+        # simple page
+        if context_name == "page"
+          return ["#{inner}", "", ""] if wiki_page_name == inner
+          return ["#{inner}", " -> ", "#{wiki_page_name}"]
+        end
+        # group page
+        if context_name != page_name
+          return ["#{context_name}", " / ", "#{wiki_page_name}"] if wiki_page_name == inner
+          return ["#{inner}", " -> ", "#{wiki_page_name}"] if context_name == "page"
+          return ["#{inner}", " -> ", "#{context_name} / #{wiki_page_name}"]
+        end 
+        if inner == page_name || inner == wiki_page_name || inner == wiki_page_name.gsub(/\s/,"-")
+          return ["#{wiki_page_name}", "", ""]
+        end
+        # fall back
+        return ["#{inner}", " -> ", "#{href}"]
+      end
+      fill_with
+    end
   end
 end
--- test/test_greencloth.rb
+++ test/test_greencloth.rb
@@ -6,8 +6,108 @@ class RainbowCloth::GreenClothTest < Test::Unit::TestCase
   end
 
   context "Converting HTML to greencloth" do
-    test "converts nested tags" do
-      assert_renders_greencloth "h2. _this is *very* important_\n", "<h2><em>this is <strong>very</strong> important</em></h2>"
+    test "convert a link to a wiki page inside a paragraph" do
+      html = "<p>this is a <a href='/page/plain-link'>plain link</a> in some text</p>"
+      greencloth = "this is a [plain link] in some text\n"
+      assert_renders_greencloth greencloth, html 
+    end
+
+    test "convert a link to a wiki page with namespace" do
+      html= "<p>this is a <a href='/namespaced/link'>link</a> in some text</p>"
+      greencloth = "this is a [namespaced / link] in some text\n"
+      assert_renders_greencloth greencloth, html 
+    end
+    
+    test "convert a link to a wiki page" do
+      html= "<p>this is a <a href='/page/something-else'>link to</a> in some text</p>"
+      greencloth = "this is a [link to -> something else] in some text\n"
+      assert_renders_greencloth greencloth, html 
+    end
+
+    test "convert a link to a wiki page with namespace and text different than link dest" do
+      html= "<p>this is a <a href='/namespace/something-else'>link to</a> in some text</p>"
+      greencloth = "this is a [link to -> namespace / something else] in some text\n"
+      assert_renders_greencloth greencloth, html 
+    end
+    
+    test "convert a link to an absolute path" do
+      html = "<p>this is a <a href='/an/absolute/path'>link to</a> in some text</p>"
+      greencloth = "this is a [link to -> /an/absolute/path] in some text\n"
+      assert_renders_greencloth greencloth, html 
+    end
+    
+    test "convert a link to an external domain" do
+      html = "<p>this is a <a href='https://riseup.net'>link to</a> a url</p>"
+      greencloth = "this is a [link to -> https://riseup.net] a url\n"
+      assert_renders_greencloth greencloth, html 
+    end
+    
+    test "a link to an external domain with the same text as dest" do
+      html = "<p>url in brackets <a href='https://riseup.net/'>riseup.net</a></p>"
+      greencloth = "url in brackets [riseup.net -> https://riseup.net/]\n"
+      assert_renders_greencloth greencloth, html 
+    end
+    
+    test "a link to a wiki page with the same name as dest" do
+      html = "<p>a <a href='/page/name-link'>name link</a> in need of humanizing</p>"
+      greencloth = "a [name link] in need of humanizing\n"
+      assert_renders_greencloth greencloth, html 
+    end
+
+    test "link to a user blue" do
+      html = "<p>link to a user <a href='/blue'>blue</a></p>"
+      greencloth = "link to a user [blue]\n"
+      assert_renders_greencloth greencloth, html 
+    end
+    
+    test "link with dashes should keep the dashes" do
+      html = "<p><a href='/-dashes/in/the/link-'>link to</a></p>"
+      greencloth = "[link to -> /-dashes/in/the/link-]\n"
+      assert_renders_greencloth greencloth, html 
+    end
+
+    test "link with underscores should keep the underscores" do
+      html = "<p>links <a href='/page/with_underscores'>with_underscores</a> should keep underscore</p>"
+      greencloth = "links [with_underscores] should keep underscore\n"
+      assert_renders_greencloth greencloth, html 
+    end
+    
+    test "a link inside a li element" do
+      html ="<ul>\n<li>\n\t\t\n<a href='/page/this'>link to</a></li></ul>"
+      greencloth = "* [link to -> this]\n"
+      assert_renders_greencloth greencloth, html 
+    end
+
+    test "an external link inside a li element" do
+      html = "<ul>\n<li><a href='https://riseup.net/'>riseup.net</a></li>\n</ul>"
+      greencloth = "* [riseup.net -> https://riseup.net/]\n"
+      assert_renders_greencloth greencloth, html 
+    end
+
+    test "many anchors inside a paragraph" do
+      html = "<p>make anchors <a name='here'>here</a> or <a name='maybe-here'>maybe here</a> or <a name='there'>over</a></p>"
+      greencloth = "make anchors [# here #] or [# maybe here #] or [# over -> there #]\n"
+      assert_renders_greencloth greencloth, html 
+    end
+
+    # TODO: there are differents in this test about how cg support writing anchors
+    # this is a reduced support of it
+    test "anchors and links" do
+      html = "<p>link to <a href='/page/anchors#like-so'>anchors</a> or <a href='/page/like#so'>maybe</a> or <a href='#so'>just</a> or <a href='#so'>so</a></p>"
+      greencloth = "link to [anchors -> anchors#like so] or [maybe -> like#so] or [just -> #so] or [so -> #so]\n"
+      assert_renders_greencloth greencloth, html 
+    end
+
+    test "more anchors" do
+      html = "<p><a href='#5'>link</a> to a numeric anchor <a name='5'>5</a></p>"
+      greencloth = "[link -> #5] to a numeric anchor [# 5 #]\n"
+      assert_renders_greencloth greencloth, html 
+    end
+
+    test "3 links without /" do
+      html = "<p><a href='some'>some</a> and <a href='other'>other</a> and <a href='one_more'>one_more</a></p>"
+      greencloth = "[some] and [other] and [one_more]\n"
+      assert_renders_greencloth greencloth, html 
     end
   end
 end
