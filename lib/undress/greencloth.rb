507c5a61e67a7363b35ab2aed0ba45f9526c998a
Álvaro Gil <zevarito@gmail.com>
2009-07-16 21:43:20 -0300

    rafactoring links a little

--- lib/undress/greencloth.rb
+++ lib/undress/greencloth.rb
@@ -20,7 +20,7 @@ module Undress
 
     # inline elements
     rule_for(:a) {|e|
-      process_links_and_anchors(e)
+      "#{process_links_and_anchors(e)}"
     }
 
     # lists
@@ -83,49 +83,56 @@ module Undress
 
       # is an anchor? and cannot be child of any h1..h6
       if name && !e.parent.name.match(/^h1|2|3|4|5|6$/)
-        fill_with = inner == name || inner == name.gsub(/-/,"\s") ? ["# ", "#{inner}", " #"] : ["# #{inner}", " -> ", "#{name} #"]
-      # is a link to an anchor?
-      elsif href && href =~ /^\/#/
-        fill_with = ["\"#{inner}\"", ":", "#{href}"]
-      elsif href && href =~ /^#/
-        fill_with = ["#{inner}", " -> ", "#{href}"]
-      # it is an external link?
-      elsif href && href =~ /^(https?|s?ftp):\/\//
-        fill_with = href.gsub(/^(https?|s?ftp):\/\//, "") == inner ? ["#{href}", "", ""] : ["#{inner}", " -> ", "#{href}"]
-      # links starting without /
-      elsif href && href =~ /^[^\/]/
-        fill_with = ["", "#{e.inner_text}", ""]
-      # link with 3 more /
-      elsif href && href.count("/") >= 3
-        fill_with = ["#{inner}", " -> ", "#{href}"]
-      # wiki page with index
-      elsif href && href =~ /(?:\/page\/\+)[0-9]+$/
-        fill_with = ["#{inner}", " -> ", "+#{href.gsub(/\+[0-9]+$/)}"]
-      # pages or group pages
+        inner == name || inner == name.gsub(/-/,"\s") ? "[# #{inner} #]" : "[# #{inner} -> #{name} #]"
+      # is a link?
+      elsif href && href != ""
+        case href
+          when /^\/#/
+            "[\"#{inner}\":#{href}"
+          when /^#/
+            "[#{inner} -> #{href}]"
+          when /^(https?|s?ftp):\/\//
+            href.gsub(/^(https?|s?ftp):\/\//, "") == inner ? "[#{href}]" : "[#{inner} -> #{href}]"
+          when /^[^\/]/
+            "[#{e.inner_text}]"
+          when /^\/.[^\/]*\/.[^\/]*\//
+            "[#{inner} -> #{href}]"
+          when /(?:\/page\/\+)[0-9]+$/
+            "[#{inner} -> +#{href.gsub(/\+[0-9]+$/)}]"
+          else
+            process_as_wiki_link(e)
+        end
       else
-        context_name, page_name = href.split("/")[1..2]
-        page_name = context_name if page_name.nil?
-        wiki_page_name = page_name.gsub(/[a-z-]*[^\/]$/m) {|m| m.tr('-',' ')}
-
-        # simple page
-        if context_name == "page"
-          return "[#{inner}]" if wiki_page_name == inner
-          return "[#{inner} -> #{wiki_page_name}]"
-        end
-        # group page
-        if context_name != page_name
-          return "[#{context_name} / #{wiki_page_name}]" if wiki_page_name == inner
-          return "[#{inner} -> #{wiki_page_name}]" if context_name == "page"
-          return "[#{inner} -> #{context_name} / #{wiki_page_name}]"
-        end 
-        if inner == page_name || inner == wiki_page_name || inner == wiki_page_name.gsub(/\s/,"-")
-          return "[#{wiki_page_name}]"
-        end
-        # fall back
-        return "[#{inner} -> #{href}]"
+        ""
       end
-      "[%s%s%s]" % fill_with
     end
+
+    def process_as_wiki_link(e)
+      inner, name, href = e.inner_html, e.get_attribute("name"), e.get_attribute("href")
+
+      # pages or group pages
+      context_name, page_name = href.split("/")[1..2]
+      page_name = context_name if page_name.nil?
+      wiki_page_name = page_name.gsub(/[a-z-]*[^\/]$/m) {|m| m.tr('-',' ')}
+
+      # simple page
+      if context_name == "page"
+        return "[#{inner}]" if wiki_page_name == inner
+        return "[#{inner} -> #{wiki_page_name}]"
+      end
+      # group page
+      if context_name != page_name
+        return "[#{context_name} / #{wiki_page_name}]" if wiki_page_name == inner
+        return "[#{inner} -> #{wiki_page_name}]" if context_name == "page"
+        return "[#{inner} -> #{context_name} / #{wiki_page_name}]"
+      end 
+      if inner == page_name || inner == wiki_page_name || inner == wiki_page_name.gsub(/\s/,"-")
+        return "[#{wiki_page_name}]"
+      end
+      # fall back
+      return "[#{inner} -> #{href}]"
+    end
+
   end
   add_markup :greencloth, GreenCloth 
 end
